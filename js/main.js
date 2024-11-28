3801d1d7f0ccf8f468ff3dbeea0d85684a8f1956
chaima1111 <chaimacheniti19@gmail.com>
2024-11-28 11:47:24 +0100

    home page

---
+++ js/main.js
@@ -0,0 +1,323 @@
+function getRandomNumberGreaterThan10() {
+  return Math.floor(Math.random() * 10000000000) + 11; // Range: 11 to 100
+}
+ globalThis.productsWomen = [
+    {"id":getRandomNumberGreaterThan10(),
+    "img":"./assets/women/product5/Le-Dino-Lisse-Noir-01.jpg",
+    "images":["./assets/women/product5/Le-Dino-Lisse-Noir-02.jpg ","../assets/women/product5/Le-Dino-Lisse-Noir-02.jpg"],
+"name":"Le Mini Dalia Black Liégé",
+"desc":"'Baguette' bag created in collaboration with Wendy Swan. Inspired by the Parisian and minimalist style of Wendy, this bag is constructed with 3 flexible gussets allowing a good capacity. Two shoulder straps for shoulder or cross-body carry. Optional short shoulder strap. XL smartphone compatible (ex iPhone 13 pro Max). Secure turnstile clasp, brass. Handmade in Italy.",
+"price":440.00,
+"color":[1,2,3],
+},
+    {"id":getRandomNumberGreaterThan10(),
+    "img":"./assets/women/product4/Le-Dino-Lisse-Ivoire-01.jpg",
+    "images":["../assets/women/product4/Le-Dino-Lisse-Ivoire-02.jpg ","../assets/women/product4/Le-Dino-Lisse-Ivoire-03.jpg"],
+"name":"Le Mini Dalia Black Liégé",
+"desc":"'Baguette' bag created in collaboration with Wendy Swan. Inspired by the Parisian and minimalist style of Wendy, this bag is constructed with 3 flexible gussets allowing a good capacity. Two shoulder straps for shoulder or cross-body carry. Optional short shoulder strap. XL smartphone compatible (ex iPhone 13 pro Max). Secure turnstile clasp, brass. Handmade in Italy.",
+"price":440.00,
+"color":[1,2,3],
+},
+    {"id":getRandomNumberGreaterThan10(),
+    "img":"./assets/women/product3/Grand-Nova-Lisse-Ivoire-013.jpg",
+    "images":["../assets/women/product3/Grand-Nova-Lisse-Ivoire-02.jpg","../assets/women/product3/Grand-Nova-Lisse-Ivoire-3.jpg"],
+"name":"Le Mini Dalia Black Liégé",
+"desc":"'Baguette' bag created in collaboration with Wendy Swan. Inspired by the Parisian and minimalist style of Wendy, this bag is constructed with 3 flexible gussets allowing a good capacity. Two shoulder straps for shoulder or cross-body carry. Optional short shoulder strap. XL smartphone compatible (ex iPhone 13 pro Max). Secure turnstile clasp, brass. Handmade in Italy.",
+"price":380.00,
+"color":[1,2,4],
+},
+    {"id":getRandomNumberGreaterThan10(),
+    "img":"./assets/women/product6/Le-Mini-Dalia-Lisse-Ivoire-01.jpg",
+    "images":["../assets/women/product6/Le-Mini-Dalia-Lisse-Ivoire-02.jpg ","../assets/women/product6/Le-Mini-Dalia-Lisse-Ivoire-03 .jpg"],
+"name":"Le Mini Dalia Black Liégé",
+"desc":"'Baguette' bag created in collaboration with Wendy Swan. Inspired by the Parisian and minimalist style of Wendy, this bag is constructed with 3 flexible gussets allowing a good capacity. Two shoulder straps for shoulder or cross-body carry. Optional short shoulder strap. XL smartphone compatible (ex iPhone 13 pro Max). Secure turnstile clasp, brass. Handmade in Italy.",
+"price":380.00,
+"color":[1,2,4],
+},
+];
+let main_prod = document.querySelector(".products")
+console.log('main_prod: ', main_prod);
+let cartItems = document.getElementById("cartItems");
+let cartTotal = document.getElementById("cartTotal");
+addProduct(productsWomen)
+let cart = [];
+
+//chech if there is products in localstorage
+if (localStorage.getItem("cart")) {
+    cart =JSON.parse(localStorage.getItem("cart"))
+}
+getDataLocalStr();
+function addProduct (products){
+
+    for (let i = 0; i < products.length; i++) {
+
+        let imgProduct = document.createElement("img");
+        let divProduct = document.createElement("div");
+        let btnCont = document.createElement("div");
+        let nameProduct =document.createElement("h3");
+        let priceProducnt = document.createElement("h6");
+        let more = document.createElement("button");
+        let btnProducnt = document.createElement("i");
+        let nameText =document.createTextNode(`${products[i].name}`);
+        let priceText =document.createTextNode(`$${products[i].price}`);
+        let moreTxt =document.createTextNode("show more");
+        
+        imgProduct.src= products[i].img;
+        nameProduct.appendChild(nameText);
+        priceProducnt.appendChild(priceText);
+        more.appendChild(moreTxt);
+        
+        btnProducnt.classList.add("fa-solid", "fa-plus")  
+        divProduct.setAttribute("class","product")
+        divProduct.setAttribute("data-id",products[i].id)
+        btnCont.setAttribute("class", "product_add")
+        btnCont.setAttribute("data-id", products[i].id)
+        imgProduct.setAttribute("class","product_img")
+        nameProduct.setAttribute("class","product_name")
+        priceProducnt.setAttribute("class", "product_price")
+        more.setAttribute("class", "show_more")
+        
+        btnCont.appendChild(btnProducnt)
+        divProduct.appendChild(imgProduct)
+        divProduct.appendChild(nameProduct)
+        divProduct.appendChild(priceProducnt)
+        divProduct.appendChild(btnCont)
+        divProduct.appendChild(more)
+        main_prod.appendChild(divProduct)
+    }
+
+}
+
+// Show user cart 
+let userCart = document.getElementById("userCart");
+let addToCartBtn = document.querySelectorAll(".product_add");
+let productCart = document.querySelectorAll(".product");
+let closeBtn = document.getElementById("closeBtn");
+let showBtn = document.querySelector(".show_cart")
+
+
+function addCart(products) {
+    for (let i = 0; i < addToCartBtn.length; i++) {
+        addToCartBtn[i].addEventListener('click', () => {
+            //verify if the element already exist
+            const findItem = cart.find(item => item.id == addToCartBtn[i].getAttribute("data-id"))
+            if (findItem) {
+                findItem.quantity += 1;
+            } else {
+                let id = products[i].id
+                let img = products[i].img
+                let name = products[i].name;
+                let price = products[i].price
+                cart.push({
+                    id,
+                    img,
+                    name,
+                    price,
+                    quantity: 1,
+                })
+            
+            }
+            addDataToLocalSFrom(cart)
+            updateCart(cart)
+            showCart()
+        })
+
+    }
+}
+function addCart2(products) {
+    for (let i = 0; i < products.length; i++) {
+      
+            //verify if the element already exist
+            const findItem = cart.find(item => item.id == products[i].getAttribute("data-id"))
+            if (findItem) {
+                findItem.quantity += 1;
+            } else {
+                let id = products[i].id
+                let img = products[i].img
+                let name = products[i].name;
+                let price = products[i].price
+                cart.push({
+                    id,
+                    img,
+                    name,
+                    price,
+                    quantity: 1,
+                })
+            
+            }
+            addDataToLocalSFrom(cart)
+            updateCart(cart)
+            showCart()
+        
+
+    }
+}
+addCart(productsWomen)
+
+function showCart() {
+    userCart.classList.add('show');
+}
+
+function hideCart() {
+    userCart.classList.remove('show');
+}
+//removing cart if i click outside
+window.addEventListener('click', (e) => {
+    if (e.target === userCart) {
+        hideCart();
+    }
+});
+closeBtn.addEventListener('click',hideCart);
+
+
+function updateCart(cart) {
+    if (cart.length === 0) {
+        cartItems.innerHTML = '<p class="text-gray-500 text-center py-4">Your cart is empty</p>';
+    }else{
+    cartItems.innerHTML= cart.map(item=>`        
+        <div class="user_cart-item" data-id=${item.id}>
+            <div>
+                 <img src="${item.img}" class ="cart_img" alt="">
+                <h3>${item.name}</h3>
+                <p>Quantity: ${item.quantity}</p>
+                </div>
+                <div>  
+                <p>$${(item.price * item.quantity).toFixed(2)}</p>
+                <button 
+                        class="remove-item remove_pro" 
+                        aria-label="Remove item" data-id=${item.id} >Remove</button>
+            </div>
+           <hr>
+        </div>  `).join('');
+
+    
+    }
+
+
+    const total = cart.reduce((sum,item)=>{return sum+(item.price * item.quantity)},0)
+    cartTotal.textContent = total.toFixed(2);
+ let items = document.getElementsByClassName("user_cart-item")
+for (let i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", (e) => {
+        if (e.target.classList.contains("remove-item")) {
+            removeFromCart(e.target.parentElement.parentElement.getAttribute("data-id"))        
+            e.target.parentElement.parentElement.remove()
+        }
+    })
+}
+}
+   
+
+function removeFromCart(id) {
+
+    cart = cart.filter((item) => {
+        return item.id !=id
+    });
+    updateCart(cart);
+   addDataToLocalSFrom(cart)
+}
+showBtn.onclick=()=>{
+    showCart()
+}
+
+//local storage
+function getDataLocalStr() {
+    let data = window.localStorage.getItem("cart");
+    if (data) {
+        //array has data in local storage
+        let arr = JSON.parse(data) 
+        updateCart(arr)
+    }
+}
+function addDataToLocalSFrom(cart) {
+    window.localStorage.setItem("cart",JSON.stringify(cart))
+}
+let productWomen = document.querySelectorAll(".product")
+
+
+
+
+
+//add image page desc
+// let showProducts = document.querySelectorAll(".product");
+
+
+// function showDes(products) {
+//         for (let i = 0; i < products.length; i++) {
+//         products[i].addEventListener("click", function (e) {
+//             // console.log(products[i].parentElement)
+//             if (e.target.classList.contains("show_more")) {
+//                 console.log('products[i]: ', products[i]);
+//                 location.href = "../showPr.html"
+
+//                 create_show(products[i])
+//             }
+//         })
+//     }
+// }
+
+// let elemenContShow = document.querySelector(".element_container-show")
+// console.log('elemenContShow: ', elemenContShow);
+
+// function create_show(product) {
+//       elemenContShow.innerHTML=` <div class="desc_product-images-gallery">
+//     <div class="desc_product-img">
+//             <img src=${product["img"]} alt="">
+//             <img class="images_product" src=${product["images"][1]} alt="">
+//             <img class="images_product" src=${product["images"][1]} alt="">
+//         </div>
+
+//     <div class="desc_product-img-main">
+//                     <img src=${product["img"]} alt="">
+//                 </div>
+//     </div>
+// <!-- product info section -->
+//        <div class="product_info">
+//        <div class="desc_product-images-txt">
+//             <h3>${product["name"]}</h3>
+//             <div class="rating-price">
+//                 <h6>$${product["price"]}.00</h6>
+//                 <div class="rating">
+//                 <i class="fa-solid fa-star"></i>
+//                 <i class="fa-solid fa-star"></i>
+//                 <i class="fa-solid fa-star"></i>
+//                 <i class="fa-regular fa-star"></i>
+//                 <i class="fa-regular fa-star"></i>
+//                 </div>
+//             </div>
+//        </div>
+//        <!-- <hr> -->
+//        <div class="product_colors">
+//             <div class="color color${product["color"][0]}"></div>
+//             <div class=" color color${product["color"][1]}"></div>
+//             <div class="color color${product["color"][2]}"></div>
+//        </div>
+//        <div  class="desc_product-images-desc">
+//         <p>${product["desc"]}</p>
+//        </div>
+//        <div class="div_show_add_cart">
+//        <button class="show_add_cart">Add To CART</button>
+//     </div>
+//     </div>`
+        
+// }
+
+// create_show(productsWomen[1])
+
+
+
+
+
+
+
+//funciton to be amel
+function hideBtn(products) {
+    let btn = document.querySelectorAll(".product_add")
+    for (let i = 0; i < products.length; i++) {
+         products[i].addEventListener("mouseover", function (e) {
+            btn[i].classList.toggle("button-visible")
+            console.log(btn[i])
+        })
+    }
+}
+
---
+++ js/product.js
@@ -0,0 +1,120 @@
+let women_proBtn = document.querySelectorAll(".show_more")
+let women_pro = document.querySelectorAll(".product")
+let search = document.querySelector(".link_search")
+// let elemenContShow = document.querySelector(".element_container-show")
+
+let elementsContShow = document.querySelector(".elements_container-show")
+
+console.log(elementsContShow)
+function searchShow(){
+search.addEventListener("click", (e) => {
+    if (e.target.classList.contains("link_search-icon")) {
+        let inputSearch = document.createElement("input")
+        inputSearch.classList.add("search_input")
+        inputSearch.setAttribute("type", "text")
+        search.appendChild(inputSearch)
+    }
+}, {once : true})
+}
+//change main image when mouse overing
+function changeImg(products) {
+    let image = document.querySelectorAll(".product_img")
+    for (let i = 0; i < products.length; i++) {
+        products[i].addEventListener("mouseover", (e) => {
+            // console.log(products[i].parentElement)
+            // location.href="../showPr.html"
+            if (e.target.classList.contains("product_img")) {
+                image[i].src=`${productsWomen[i]["images"][0]}`
+            }
+        })
+        products[i].addEventListener("mouseout", (e) => {
+            // console.log(products[i].parentElement)
+            // location.href="../showPr.html"
+            if (e.target.classList.contains("product_img")) {
+                image[i].src=`${productsWomen[i]["img"]}`
+                console.log('productsWomen: ', productsWomen[i]["images"][1]);
+            }
+        })
+        
+    }
+}
+
+console.log(women_pro)
+function showDes(products) {
+
+        for (let i = 0; i < productsWomen.length; i++) {
+        
+        products[i].addEventListener("click", function (e) {
+
+            console.log('products[i].parentElement: ', products[i].parentElement);
+            
+                if (e.target.classList.contains("show_more")) {
+                // location.href = "../showPr.html"
+
+            }
+        })
+    }
+
+
+}
+
+function create_show(product) {
+    let elemenContShow = document.createElement("div")
+elemenContShow.classList.add("element_container-show","show_hidden")
+    elemenContShow.innerHTML = ` <div class="desc_product-images-gallery">
+    <div class="desc_product-img">
+            <img src=${product["img"]} alt="">
+            <img class="images_product" src=${product["images"][0]} alt="">
+            <img class="images_product" src=${product["images"][0]} alt="">
+        </div>
+
+    <div class="desc_product-img-main">
+                    <img src=${product["img"]} alt="">
+                </div>
+    </div>
+<!-- product info section -->
+       <div class="product_info">
+       <div class="desc_product-images-txt">
+            <h3>${product["name"]}</h3>
+            <div class="rating-price">
+                <h6>$${product["price"]}.00</h6>
+                <div class="rating">
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-solid fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                </div>
+            </div>
+       </div>
+       <!-- <hr> -->
+       <div class="product_colors">
+            <div class="color color${product["color"][0]}"></div>
+            <div class=" color color${product["color"][1]}"></div>
+            <div class="color color${product["color"][2]}"></div>
+       </div>
+       <div  class="desc_product-images-desc">
+        <p>${product["desc"]}</p>
+       </div>
+       <div class="div_show_add_cart">
+       <button class="show_add_cart">Add To CART</button>
+    </div>
+    </div>`;
+    elementsContShow.appendChild(elemenContShow)
+        
+}
+
+
+changeImg(women_pro)
+
+for (let i = 0; i < productsWomen.length; i++) {
+create_show(productsWomen[i])
+}
+// create_show(productsWomen[0])
+// create_show(productsWomen[2])
+// 
+// showDes(women_pro)
+console.log('women_pro: ', productsWomen);
+// showDes(women_pro)
+
+
